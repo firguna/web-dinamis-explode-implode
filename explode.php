9afea74f738d1ba32bee137f1eb8b735114eae25
firguna <44139623+firguna@users.noreply.github.com>
2019-03-25 11:45:59 +0700

    Create explode.php

---
+++ explode.php
@@ -0,0 +1,16 @@
+<?php 
+$universitas = "UIN,UGM,UNY";
+$explode_universitas = explode(",", $universitas);
+
+$nama="Fira Guna Cahya Vernanda";
+$explode_nama = explode(" ", $nama);
+
+$tanggal_lahir = "15:Juni:2001";
+$explode_tanggal_lahir = explode(":", $tanggal_lahir);
+
+$hari = "Senin-Selasa-Rabu";
+$explode_hari = explode("-", $hari);
+
+$buah = "Semangka,Apel,Anggur";
+$explode_buah = explode(",", $buah);
+?>
